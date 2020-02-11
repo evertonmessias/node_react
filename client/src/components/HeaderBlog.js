b7957ccfb30d169b806637264996f8129e671667
evertonmessias <everton.messias@gmail.com>
2020-02-10 23:22:43 -0300

    10-02-2020_23:22:18

--- client/src/components/HeaderBlog.js
+++ client/src/components/HeaderBlog.js
@@ -1,10 +1,33 @@
-import React from 'react';
-import {} from 'carbon-components-react'
+import React, { Component } from 'react';
+import { BrowserRouter, Link, Switch, Route } from 'react-router-dom';
+import logo from '../logo.svg';
+import Categorias  from './Categorias';
+import Posts from './Posts';
+import Home from './Home';
 
-const HeaderBlog = props => {
+class HeaderBlog extends Component {
+  render() {
     return (
-      <p>OII HEADER</p>
+      <div className="site">        
+        <BrowserRouter>
+        <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <nav className="menu"> 
+        <Link to='/home' className="btn"> Home </Link>
+        <Link to='/categorias' className="btn"> Categorias </Link>
+        <Link to='/posts' className="btn"> Posts </Link>
+        </nav>
+        </header>
+        <main className="main">
+        <Switch>
+          <Route path='/home' component={ Home } />
+          <Route path='/categorias' component={ Categorias } />
+          <Route path='/posts' component={ Posts } />
+        </Switch>
+        </main>
+        </BrowserRouter>
+      </div>
     );
   }
-
+}
 export default HeaderBlog;
